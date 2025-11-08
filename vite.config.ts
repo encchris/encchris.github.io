8ff5e48f45281ed941998b0bfcca9f3f98318447
encchris <encchris90@gmail.com>
2025-11-08 14:58:21 -0300

    re build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = 'encchris.github.io';
-
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: `/`,
   plugins: [
     react(),
     tailwindcss()
